1154d77c5dd8f5915ef93d20753b399d06b70942
elgedr <elgedr@ttu.ee>
2021-09-27 18:50:22 +0300

    commit1

---
+++ server/src/test/java/com/freesia/server/controller/ShipsControllerTest.java
@@ -0,0 +1,56 @@
+package com.freesia.server.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.freesia.server.controllers.ShipController;
+import com.freesia.server.models.Ship;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ShipsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void allShips() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ship/all"))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        List<Ship> ship = objectMapper.readValue(contentAsString, new TypeReference<>() {
+        });
+        assertEquals(3, ship.size());
+    }
+
+    @Test
+    void shipById() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/ship/2"))
+                .andExpect(status()
+                        .isOk())
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        Ship ship = objectMapper.readValue(contentAsString, new TypeReference<>() {
+        });
+        assertEquals("Ellina", ship.getName());
+    }
+
+}
