c2591c9ac76b7bf751719d596839f981a04d17f6
Juliadjomina <judjom@ttu.ee>
2021-09-28 18:00:48 +0300

    Updated sql file.

--- server/db_table.sql
+++ server/db_table.sql
@@ -1,59 +1,44 @@
+CREATE TABLE Flight
+(
+    id                INT AUTO_INCREMENT NOT NULL,
+    departure_place   VARCHAR(50) NOT NULL,
+    departure_time    TIMESTAMP   NOT NULL,
+    destination_place VARCHAR(50) NOT NULL,
+    arrival_time      TIMESTAMP   NOT NULL,
+    CONSTRAINT pk_flight PRIMARY KEY (id)
+);
+
 CREATE TABLE Ship
 (
     id            INT AUTO_INCREMENT NOT NULL,
+    flight_id     INT         NOT NULL,
     name          VARCHAR(50) NOT NULL,
     num_of_places INT         NOT NULL,
-    CONSTRAINT pk_ship PRIMARY KEY (id)
+    CONSTRAINT pk_ship PRIMARY KEY (id),
+    CONSTRAINT fk_flight_id FOREIGN KEY (flight_id) REFERENCES Flight (id)
 );
 
 CREATE TABLE Place
 (
-    id        INT AUTO_INCREMENT NOT NULL,
-    type      VARCHAR(50) NOT NULL,
-    price     INT         NOT NULL,
-    is_booked BOOLEAN DEFAULT FALSE,
-    CONSTRAINT pk_place PRIMARY KEY (id)
+    id           INT AUTO_INCREMENT NOT NULL,
+    ship_id      INT         NOT NULL,
+    passenger_id INT,
+    type         VARCHAR(50) NOT NULL,
+    price        INT         NOT NULL,
+    place_num    INT         NOT NULL,
+    CONSTRAINT pk_place PRIMARY KEY (id),
+    CONSTRAINT fk_ship_id FOREIGN KEY (ship_id) REFERENCES Ship (id),
+    CONSTRAINT fk_passenger_id FOREIGN KEY (passenger_id) REFERENCES Passenger (id)
 );
 
-CREATE TABLE Ship_Place_Combination
-(
-    ship_id  INT NOT NULL,
-    place_id INT NOT NULL,
-    CONSTRAINT pk_ship_place_combination PRIMARY KEY (ship_id, place_id),
-    CONSTRAINT fk_ship_place_combination_ship_id FOREIGN KEY (ship_id) REFERENCES Ship (id),
-    CONSTRAINT fk_ship_place_combination_place_id FOREIGN KEY (place_id) REFERENCES Place (id)
-);
-
-
-CREATE TABLE Ship_Places_Availability
-(
-    ship_id                 INT NOT NULL,
-    num_of_available_places INT NOT NULL,
-    CONSTRAINT fk_ship_places_availability_ship_id FOREIGN KEY (ship_id) REFERENCES Ship (id)
-);
-
-CREATE TABLE Reservation
+CREATE TABLE Passenger
 (
     id                INT AUTO_INCREMENT NOT NULL,
-    ship_id           INT         NOT NULL,
-    place             INT         NOT NULL,
     passenger_name    VARCHAR(50) NOT NULL,
     passenger_surname VARCHAR(50) NOT NULL,
     email             VARCHAR(65) NOT NULL,
     telephone         VARCHAR(8)  NOT NULL,
     CONSTRAINT pk_reservation PRIMARY KEY (id),
-    CONSTRAINT fk_reservation_ship_id FOREIGN KEY (ship_id) REFERENCES Ship (id),
     CONSTRAINT uq_passenger_data UNIQUE (passenger_name, passenger_surname, email, telephone)
 );
 
-CREATE TABLE Flight
-(
-    id                INT AUTO_INCREMENT NOT NULL,
-    ship_id           INT         NOT NULL,
-    departure_place   VARCHAR(50) NOT NULL,
-    departure_time    TIMESTAMP   NOT NULL,
-    destination_place VARCHAR(50) NOT NULL,
-    arrival_time      TIMESTAMP   NOT NULL,
-    CONSTRAINT pk_flight PRIMARY KEY (id),
-    CONSTRAINT fk_flight_ship_id FOREIGN KEY (ship_id) REFERENCES Ship (id)
-);
