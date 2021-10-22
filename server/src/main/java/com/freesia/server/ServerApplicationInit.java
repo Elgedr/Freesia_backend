bf62879247cf0c8ae91bed6fc87b6e36beb86248
elgedr <elgedr@ttu.ee>
2021-10-22 21:52:51 +0300

    Gradle dependencies are updated.

--- server/src/main/java/com/freesia/server/ServerApplicationInit.java
+++ server/src/main/java/com/freesia/server/ServerApplicationInit.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class ServerApplicationInit implements CommandLineRunner {
     @Autowired
