5e70095963a2958e03adc6d88b591872019ce0e3
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-10-25 12:06:15 +0300

    make junit 5 tests work

--- server/src/test/java/com/freesia/server/controller/ShipsControllerTest.java
+++ server/src/test/java/com/freesia/server/controller/ShipsControllerTest.java
@@ -3,25 +3,22 @@ package com.freesia.server.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.freesia.server.models.Ship;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class ShipsControllerTest {
 
     @Autowired
