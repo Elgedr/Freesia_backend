9d971e3b3a663160fe6f9025ff0461c7c9f187b3
Juliadjomina <judjom@ttu.ee>
2021-11-15 16:50:21 +0300

    /api

--- src/main/java/com/freesia/server/controllers/ExampleController.java
+++ src/main/java/com/freesia/server/controllers/ExampleController.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin
-@RequestMapping("/freesia")
+@RequestMapping("/api/freesia")
 @RestController
 public class ExampleController {
 
--- src/main/java/com/freesia/server/controllers/ShipController.java
+++ src/main/java/com/freesia/server/controllers/ShipController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/ships")
+@RequestMapping("/api/ships")
 @RestController
 @CrossOrigin
 public class ShipController {
