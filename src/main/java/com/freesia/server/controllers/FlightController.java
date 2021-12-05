0398cd0139e063f55c4a2b15d77c2fa5c5858864
Gleb Komissarov <glkomi@ttu.ee>
2021-12-05 12:38:42 +0000

    Update FlightController.java

--- src/main/java/com/freesia/server/controllers/FlightController.java
+++ src/main/java/com/freesia/server/controllers/FlightController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/flights")
+@RequestMapping({"/flights", "/flightsss"})
 @RestController
 //@CrossOrigin
 public class FlightController {
