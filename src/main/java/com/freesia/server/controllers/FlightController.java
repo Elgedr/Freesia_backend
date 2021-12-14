2c3a4b6a9303f7f0dbdfbf5db1d734c3fc41658a
elgedr <elgedr@ttu.ee>
2021-12-14 17:04:30 +0200

    commit

--- src/main/java/com/freesia/server/controllers/FlightController.java
+++ src/main/java/com/freesia/server/controllers/FlightController.java
@@ -4,11 +4,8 @@ import com.freesia.server.models.Flight;
 import com.freesia.server.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
