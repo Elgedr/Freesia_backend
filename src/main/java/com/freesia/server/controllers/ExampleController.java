ace3932a52e1dd20fc08a5ed8b0b586465ad89ab
elgedr <elgedr@ttu.ee>
2021-12-23 20:13:29 +0200

    not used imports deleted

--- src/main/java/com/freesia/server/controllers/ExampleController.java
+++ src/main/java/com/freesia/server/controllers/ExampleController.java
@@ -2,17 +2,13 @@ package com.freesia.server.controllers;
 
 
 import com.freesia.server.models.Flight;
-import com.freesia.server.repositories.FlightRepository;
 import com.freesia.server.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin
