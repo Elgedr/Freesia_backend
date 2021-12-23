c0a28171c7b9fda56832d75d408b75a706f87ff5
erika <ermaks@ttu.ee>
2021-12-23 19:11:43 +0200

    23/12: added: user controller and user service!

---
+++ src/main/java/com/freesia/server/services/UserService.java
@@ -0,0 +1,22 @@
+package com.freesia.server.services;
+
+import com.freesia.server.models.User;
+import com.freesia.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+}
